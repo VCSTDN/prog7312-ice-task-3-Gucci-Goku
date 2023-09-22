e57c2f1de19d1ed19b9db57e4396adf2059e2968
Gucci-Goku <126815235+Gucci-Goku@users.noreply.github.com>
2023-09-22 09:59:40 +0200

    Add files via upload
    Sir, this is the proper Ice task 3, I by mistake uploaded the algorithim task to this submission so bith are here. Ice Task 3 is this one.

---
+++ IceTask3 Prog/IceTask3 Prog/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceTask3_Prog
+{
+    internal class Employee
+    {
+        
+
+        public int departmentID;
+        public string empSurname;
+
+        public Employee(int departmentID, string surname)
+        {
+            this.departmentID = departmentID;
+            this.empSurname = surname;
+        }
+    }
+}
---
+++ IceTask3 Prog/IceTask3 Prog/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceTask3_Prog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;
+            string name;
+            Random ran = new Random();
+            string[] surname = { "Smith","Green","Jackson", "Blue","Robinson","Streep","Depp","White","Miller","Rock"};
+            List<Employee> list = new List<Employee>();
+
+            Random random = new Random();
+
+          
+          
+
+            for (int i = 0; i < 9; i++)
+            {
+                // Generate a random index within the range of the array's length
+                int randomIndex = random.Next(0, surname.Length);
+
+                // Use the random index to access a random string from the array
+                string randomString = surname[randomIndex];
+
+                list.Add(new Employee(ran.Next(1, 5), randomString));
+               
+            }
+
+            List<Employee> sortedList = SortEmployees(list);
+
+            foreach (var employee in sortedList)
+            {
+               
+                Console.WriteLine($"ID: {employee.departmentID}, Surname: {employee.empSurname}");
+            }
+
+
+            Console.ReadLine();
+
+        }
+        public static List<Employee> SortEmployees(List<Employee> employees)
+        {
+            return employees.OrderBy(e => e.departmentID).ThenBy(e => e.empSurname).ToList();
+        }
+
+    }
+}
